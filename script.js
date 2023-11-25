ebd83f2a3e12e53f460aa16b252f3abec0f3a554
Yulieth02 <yuliedyvon@gmail.com>
2023-11-24 21:54:01 -0500

    se termino css

--- script.js
+++
@@ -1,8 +0,0 @@
-// Código JavaScript para crear el menú hamburguesa
-
-const menuBtn = document.querySelector(".menu-btn");
-const menuItems = document.querySelectorAll(".menu-item");
-
-menuBtn.addEventListener("click", () => {
-  menuItems.classList.toggle("open");
-});
